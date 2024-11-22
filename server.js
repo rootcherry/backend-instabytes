859894f92681b6726bb96c3d09e95f14597c8594
Chris N <hi.chrisn@gmail.com>
2024-11-22 02:38:47 -0300

    dia 2

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 import express from 'express';
 
+const posts = [
+  { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
+  { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
+  { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150" },
+];
+
+function buscarPostPorID(id) {
+  return posts.findIndex((post) => {
+    return post.id === Number(id);
+  });
+}
+
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.status(200).send({});
+app.use(express.json());
+
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
+});
+
+app.get("/posts/:id", (req, res) => {
+  const index = buscarPostPorID(req.params.id);
+  res.status(200).json(posts[index]);
 });
 
 app.listen(3000, () => {
