ccaef37791107d021f93a5db9e4482370d06490d
Chris N <hi.chrisn@gmail.com>
2024-11-22 19:26:31 -0300

    add multer

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,19 @@
-import getTodosPosts from '../models/postsModel.js';
+import { getTodosPosts, criarPost } from '../models/postsModel.js';
 
 export async function listarPosts(req, res) {
   const posts = await getTodosPosts();
   res.status(200).json(posts);
+};
+
+export async function postarNovoPost(req, res) {
+  const novoPost = req.body;
+
+  try {
+    const postCriado = await criarPost(novoPost);
+
+    res.status(200).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({ 'Erro': 'Falha na requisição.' });
+  }
 };
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,18 @@
 import express from 'express';
-import { listarPosts } from '../controllers/postsController.js';
+import multer from 'multer';
+
+import { listarPosts, postarNovoPost } from '../controllers/postsController.js';
+
+const upload = multer({ dest: './uploads' });
 
 const routes = (app) => {
   app.use(express.json());
 
   app.get('/posts', listarPosts);
+
+  app.post('/posts', postarNovoPost);
+
+  app.post('/upload', upload.single('imagem'),);
 };
 
 export default routes;
